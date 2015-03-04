4122a25b76e58412ac4d3f9de936a7785cabe087
Xavier Boubert <xavier@boubert.fr>
2015-03-04 15:29:31 +0100

    feat(website): include ":converter" usage in Control Page example

--- examples/page-control.js
+++ examples/page-control.js
@@ -1,11 +1,15 @@
 window.Plumes.ready(function() {
   'use strict';
 
+  this.converter('upper', function(value) {
+    return (value + '').toUpperCase();
+  });
+
   this.controlApp.pageControl.controller(function(next) {
 
     this.bind('robot.name', function(value) {
       if(value) {
-        this.collection('img', 'http://cyborg.namedecoder.com/webimages/edox-' + value.toUpperCase() + '.png');
+        this.collection('img', 'http://cyborg.namedecoder.com/webimages/edox-' + value + '.png');
         this.el.avatar.css({
           height: '',
           background: ''
