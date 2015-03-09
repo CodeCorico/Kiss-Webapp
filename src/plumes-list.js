ca43e9db2cf18572d2d6d2de71a983602c8fbc92
Xavier Boubert <xavier@boubert.fr>
2015-03-09 17:29:33 +0100

    feat(pl-list): add virtualization mode
    Issue #5

--- src/plumes-list.js
+++ src/plumes-list.js
@@ -11,7 +11,13 @@
         _bindingParent = [],
         _bindingKey = null,
         _bindingItem = null,
-        _bindingConverter = null;
+        _bindingConverter = null,
+        _list = [],
+        _virtualization = $list.attr('virtualization');
+
+    _virtualization = _virtualization ? $.extend(true, {
+      itemHeight: 1
+    }, $.parseJSON(_virtualization)) : null;
 
     function _compare(a, b) {
       if(typeof a != typeof b) {
@@ -52,7 +58,7 @@
       });
     }
 
-    function _createMissingComponents(count, newList, lengthChanged) {
+    function _createMissingComponents(count, callback, args) {
       var component = new plumes.Component(plumes, app, page, null);
       _components.push(component);
 
@@ -61,54 +67,125 @@
         component.link({}, function($dom) {
           $list.append($dom);
 
+          if(_virtualization) {
+            $dom.css({
+              position: 'absolute',
+              top: 0,
+              left: 0,
+              width: '100%'
+            });
+          }
+
           _bindParent(component.keysBinded());
 
           count--;
           if(count < 1) {
-            return _updateList(newList, lengthChanged);
+            if(callback) {
+              callback.apply(_this, args || []);
+            }
+            return;
           }
-          _createMissingComponents(count, newList, lengthChanged);
+          _createMissingComponents(count, callback, args);
         });
       });
 
     }
 
-    function _deleteComponents(count, newList, lengthChanged) {
+    function _deleteComponents(count, callback, args) {
       for(var i = 0; i < count; i++) {
         var component = _components.pop();
         component.destroy();
         $list.children().last().remove();
       }
 
-      _updateList(newList, lengthChanged);
+      if(callback) {
+        callback.apply(_this, args || []);
+      }
     }
 
-    function _updateList(newList, lengthChanged) {
-      newList = newList || [];
-      lengthChanged = typeof lengthChanged == 'undefined' ? newList.length != _components.length : lengthChanged;
-
-      if(newList.length > _components.length) {
-        return _createMissingComponents(newList.length - _components.length, newList, lengthChanged);
-      }
-      if(newList.length < _components.length) {
-        return _deleteComponents(_components.length - newList.length, newList, lengthChanged);
+    function _updateListLayout(list, lengthChanged) {
+      if(!_virtualization) {
+        return;
       }
 
-      $.each(newList, function(i) {
-        var component = _components[i];
+      var $container = $list.parent(),
+          container = $container.get(0),
+          y = typeof container.pageYOffset != 'undefined' ? container.pageYOffset : container.scrollTop,
+          count = Math.ceil($container.height() / _virtualization.itemHeight) + 1;
 
-        if(lengthChanged) {
-          component.collection('$length', newList.length);
+      if(list) {
+        count = count.length > list.length ? list.length : count;
+
+        if(count > _components.length) {
+          return _createMissingComponents(count - _components.length, _updateListLayout, [list, lengthChanged]);
         }
-        if(component.collection('$index') != i) {
-          component.collection('$index', i);
+        if(count < _components.length) {
+          return _deleteComponents(count - list.length, _updateListLayout, [list, lengthChanged]);
+        }
+      }
+
+      count = count > _components.length ? _components.length : count;
+
+      var startListIndex = Math.floor(y / _virtualization.itemHeight);
+
+      $list.children().each(function(i) {
+        var $child = $(this),
+            component = _components[i],
+            listIndex = i + startListIndex,
+            item = _list[listIndex];
+
+        if(item) {
+          if(lengthChanged) {
+            component.collection('$length', _list.length);
+          }
+          if(component.collection('$index') != listIndex) {
+            component.collection('$index', listIndex);
+          }
+
+          component.collection(_bindingItem, plumes.extendEverything(item));
+          $child.css('display', '');
+        }
+        else {
+          $child.css('display', 'none');
         }
 
-        var item = component.collection(_bindingItem);
-        if(!_compare(this, item)) {
-          component.collection(_bindingItem, plumes.extendEverything(this));
-        }
+        $child.css('top', y + (i * _virtualization.itemHeight) - (y % _virtualization.itemHeight));
       });
+    }
+
+    function _updateList(list, lengthChanged) {
+      _list = list || [];
+      lengthChanged = typeof lengthChanged == 'undefined' ? _list.length != _components.length : lengthChanged;
+
+      if(_virtualization) {
+        $list.css('height', _virtualization.itemHeight * _list.length);
+
+        _updateListLayout(_list, lengthChanged);
+      }
+      else {
+        if(_list.length > _components.length) {
+          return _createMissingComponents(_list.length - _components.length, _updateList, [_list, lengthChanged]);
+        }
+        if(_list.length < _components.length) {
+          return _deleteComponents(_components.length - _list.length, _updateList, [_list, lengthChanged]);
+        }
+
+        $.each(_list, function(i) {
+          var component = _components[i];
+
+          if(lengthChanged) {
+            component.collection('$length', _list.length);
+          }
+          if(component.collection('$index') != i) {
+            component.collection('$index', i);
+          }
+
+          var item = component.collection(_bindingItem);
+          if(!_compare(this, item)) {
+            component.collection(_bindingItem, plumes.extendEverything(this));
+          }
+        });
+      }
     }
 
     this.compile = function() {
@@ -124,6 +201,29 @@
         .html()
         .replace(/({{(.*?)}})/g, '<span pl-html="$2"></span>');
       $list.html('');
+
+      if(_virtualization) {
+        var $container = $('<div />');
+
+        $.each($list.get(0).attributes, function(idx, attr) {
+          if(attr.nodeName != 'pl-list' && attr.nodeName != 'virtualization') {
+            $container.attr(attr.nodeName, attr.nodeValue);
+          }
+        });
+
+        $list.replaceWith($container);
+        $container.append($list);
+
+        $list.css({
+          position: 'relative',
+          height: 'auto',
+          margin: 0,
+          padding: 0
+        });
+
+        $container.css('overflow', 'auto');
+        $container.scroll(_updateListLayout);
+      }
 
       component.registerList(_this);
     };
@@ -144,6 +244,12 @@
 
       _updateList(value);
     };
+
+    if(_virtualization) {
+      $(window).resize(function() {
+        _updateListLayout();
+      });
+    }
 
   };
 
